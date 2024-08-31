38867366476679a3f0cdcb473ae5cd6841d47af6
Jonas Portela <108738826+jondelphi@users.noreply.github.com>
2024-08-31 07:41:23 -0300

    Modificado o Login

--- Control/login.php
+++ Control/login.php
@@ -4,17 +4,18 @@ $url=$dados = file_get_contents('http://localhost/apicold/getUser/'.$_POST['nick
 $dados = json_decode($dados, true);
 try{
     if (is_array($dados)) {
-        # code...
+        if (count($dados)==2) {
+            $_SESSION['ice']['pagina']='View/apontamento.php';
+            $_SESSION['ice']['user']=$dados['user'];
+            $_SESSION['ice']['nivel']=$dados['nivel'];
+        } else{
+            $_SESSION['ice']['erro']=true;
+            $_SESSION['ice']['mensagem']='Usuário não autorizado';
+        }     
     }
-if (count($dados)==2) {
-    $_SESSION['ice']['pagina']='View/apontamento.php';
-    $_SESSION['ice']['user']=$dados['user'];
-    $_SESSION['ice']['nivel']=$dados['nivel'];
-} else{
-    echo 'erro';
-}
 }catch(Exception $e){
-    echo 'erro';
+    $_SESSION['ice']['erro']=true;
+    $_SESSION['ice']['mensagem']='Usuário não autorizado';
 
 }
 
